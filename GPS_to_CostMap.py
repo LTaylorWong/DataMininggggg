a5dd7cccccdeaed6db4e45c8b7e5acbd605bb2ea
Shannon Quinn <srquinn1@gmail.com>
2019-04-20 13:33:37 -0400

    places correct colored icons map

--- GPS_to_CostMap.py
+++ GPS_to_CostMap.py
@@ -22,12 +22,50 @@ def clean_gps_data(file):
 
 
 def top_kml(file):
+    """
+    writes the header for the cost kml file
+    adds the style for the different pins needed
+    :param file: kml file pointer
+    :return: None
+    """
+    indent = '    '
+    half_indent = '  '
     file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
     file.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
     file.write('<Document>\n')
 
+    for i in range(3):
+        if i == 0:
+            file.write(indent + "<Style id='redLeftMark'>\n")
+        elif i == 1:
+            file.write(indent + "<Style id='greenRightMark'>\n")
+        else:
+            file.write(indent + "<Style id='yellowStopMark'>\n")
+
+        file.write(half_indent + indent + "<IconStyle>\n")
+        file.write(indent * 2 + '<Icon>\n')
+        if i == 0:
+            file.write(
+                indent * 2 + half_indent + '<href>http://maps.google.com/mapfiles/kml/paddle/red-stars.png</href>\n')
+        elif i == 1:
+            file.write(
+                indent * 2 + half_indent + '<href>http://maps.google.com/mapfiles/kml/paddle/grn-stars.png</href>\n')
+        else:
+            file.write(
+                indent * 2 + half_indent + '<href>http://maps.google.com/mapfiles/kml/paddle/ylw-stars.png</href>\n')
+
+
+        file.write(indent * 2 + '</Icon>\n')
+        file.write(half_indent + indent +'</IconStyle>\n')
+        file.write(indent + '</Style>\n')
+
 
 def end_kml(file):
+    """
+    writes end of kml file
+    :param file: kml file pointer
+    :return: None
+    """
     file.write('</Document>\n')
     file.write('</kml>\n')
 
@@ -43,35 +81,20 @@ def write_coordinate(coordinate, file, direction):
     """
 
     indent = "    "
-    half_indent = "  "
-    red = 'BB0B27'
-    green = '005900'
-    yellow = 'FFCA00'
 
     file.write("<Placemark>\n")
 
     if direction == 'left':
         file.write(indent + "<description> Red Pin for left turn </description>\n")
+        file.write(indent + '<styleUrl>#redLeftMark</styleUrl>\n')
     elif direction == 'right':
         file.write(indent + "<description> Green Pin for right turn </description>\n")
+        file.write(indent + '<styleUrl>#greenRightMark</styleUrl>\n')
     else:
         file.write(indent + "<description> Yellow Pin for stop </description>\n")
+        file.write(indent + '<styleUrl>#yellowStopMark</styleUrl>\n')
 
-    file.write(indent + '<Style id="normalPlacemark">\n')
-    file.write(indent + half_indent + "<IconStyle>\n")
 
-    if direction == 'left':
-        file.write(indent * 2 + "<color>%s</color>\n" % red)
-    elif direction == 'right':
-        file.write(indent * 2 + "<color>%s</color>\n" % green)
-    else:
-        file.write(indent * 2 + "<color>%s</color>\n" % yellow)
-
-    file.write(indent * 2 + "<Icon>\n")
-    file.write(indent * 2 + half_indent + "<href>http://maps.google.com/mapfiles/kml/paddle/1.png</href>\n")
-    file.write(indent * 2 + "</Icon>\n")
-    file.write(indent + half_indent + "</IconStyle>\n")
-    file.write(indent + "</Style>\n")
     file.write(indent + "<Point>\n")
     file.write(indent * 2)
     file.write("<coordinates>%f,%f</coordinates>\n" % (coordinate[0], coordinate[1]))
@@ -89,7 +112,7 @@ def main():
     with open(kml_path, 'w') as f:
         top_kml(f)
         write_coordinate((-77.59, 43.13), f, "left")
-        write_coordinate((-77.50, 43.10), f, "right")
+        write_coordinate((-77.58, 43.10), f, "right")
         write_coordinate((-77.60, 43.12), f, "stop")
         end_kml(f)
 
