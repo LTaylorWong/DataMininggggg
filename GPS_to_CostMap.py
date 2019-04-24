c18bfc35ed32fa1e7138e2dc2ed4df6c12ffb41c
Shannon Quinn <srquinn1@gmail.com>
2019-04-24 00:38:26 -0400

    oops accidently got rid of stop sign data, classifies stops again

--- GPS_to_CostMap.py
+++ GPS_to_CostMap.py
@@ -10,28 +10,22 @@ import GPS_to_KML
 def clean_gps_data(data):
     """
     cleans and parses the gps data
-    :param gps_file: file pointer to gps data
+    :param data: file pointer to gps data
     :return: cleaned data
     """
-    print(len(data))
     cleaned = []
+    i = 0
     for d in data:
         gga = d[0]
         coords = d[1]
-        fix =  int(gga[1])
+        fix = int(gga[1])
         precision = gga[2]
         if (fix <= 2 and fix != 0) and (float(precision) <= 4 and float(precision) != 0):
             coords.append(gga[0])
-            # redundant data check
-            if len(cleaned) > 0:
-                lng = coords[0]
-                lon = coords[1]
-                last = cleaned[len(cleaned) - 1]
-                if abs(float(last[0]) - lng) >= 0.0001 or abs(float(last[1]) - lon) >= 0.0001:
-                    cleaned.append(coords)
-            else:
+            if i % 10 == 0:
                 cleaned.append(coords)
-
+        i += 1
+    print(len(data), len(cleaned))
     return cleaned
 
 
@@ -47,15 +41,18 @@ def classify_coordinates(coordinates):
         prev_lon = coordinates[i - 1][1]
         prev_speed = coordinates[i - 1][3]
 
+
         next_lng = coordinates[i + 1][0]
         next_lon = coordinates[i + 1][1]
         next_speed = coordinates[i + 1][3]
+
 
         lng_threshold = abs(lng - prev_lng) <= 0.0005 and abs(lng - next_lng) <= 0.0005
         lon_threshold = abs(lon - prev_lon) <= 0.0005 and abs(lon - next_lon) <= 0.0005
         speed_threshold = speed <= 0.1 and prev_speed <= 0.1 and next_speed <= 0.1
 
         if lng_threshold and lon_threshold and speed_threshold:
+            print("Classified stop at", lng, lon, speed)
             redundant_data_check(lng, lon, classified_coordinates, 's')
 
         # Attempt to find turns, this needs to be improved though
@@ -73,22 +70,22 @@ def classify_coordinates(coordinates):
     return classified_coordinates
 
 
-def redundant_data_check(lng, lon, classified, type):
+def redundant_data_check(lng, lon, classified, group):
     """
     checks if a data point within a certain threshold has already been classified in the set
     helps to avoid writing too many points in one spot if they were there too long
     :param lng:
     :param lon:
     :param classified:
-    :param type:
+    :param group:
     :return:
     """
     if len(classified) > 0:
         last = classified[len(classified) - 1]
-        if abs(float(last[0][0]) - lng) >= 0.001 or abs(float(last[0][1]) - lon) >= 0.001 :
-            classified.append([(lng, lon), type])
+        if abs(float(last[0][0]) - lng) >= 0.001 or abs(float(last[0][1]) - lon) >= 0.001 and group != last[1]:
+            classified.append([(lng, lon), group])
     else:
-        classified.append([(lng, lon), type])
+        classified.append([(lng, lon), group])
 
 
 def top_kml(file):
@@ -172,7 +169,7 @@ def write_coordinate(coordinate, file, direction):
 
 def main():
 
-    gps_file = 'data/gps_2.txt'
+    gps_file = 'data/gps_1.txt'
 
     gps_f = open(gps_file)
     gps_data = GPS_to_KML.convert(gps_f)
